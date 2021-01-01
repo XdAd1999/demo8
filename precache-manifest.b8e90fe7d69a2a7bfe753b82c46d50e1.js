8691e01f339816c39ad02e7b6a198890c77644e4
阿栋的仓库 <2675413003@qq.com>
2021-01-01 13:46:42 +0800

    dist

---
+++ precache-manifest.b8e90fe7d69a2a7bfe753b82c46d50e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "302975e73574ea447ac0",
+    "url": "/css/app.8515c253.css"
+  },
+  {
+    "revision": "d9a3df15a63da0ad34006101d083467d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "302975e73574ea447ac0",
+    "url": "/js/app.10b7db74.js"
+  },
+  {
+    "revision": "72bc0322c0e3cdea52d2",
+    "url": "/js/chunk-vendors.117c999f.js"
+  },
+  {
+    "revision": "65975ed126ff3fb8192cd1fcb55f6983",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
